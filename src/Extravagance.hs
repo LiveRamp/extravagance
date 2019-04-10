7e0d8a1d43678e5c08928ed7bfb071c8cd588fb9
Edward Gao <edward.gao@liveramp.com>
2019-04-10 11:29:17 -0700

    formatting

--- src/Extravagance.hs
+++ src/Extravagance.hs
@@ -280,25 +280,25 @@ redactMethod patch methodName = id
 
 redactBlockStmt :: RedactionPatch -> BlockStmt -> BlockStmt
 redactBlockStmt patch blockStmt = case blockStmt of
-    (BlockStmt stmt) -> BlockStmt $ redactStmt stmt
-        where redactStmt block = case block of
-                (StmtBlock block) -> StmtBlock $ redactBlock patch block
-                (IfThen cond stmt) -> IfThen cond $ redactStmt stmt
-                (IfThenElse cond yes no) -> IfThenElse cond (redactStmt yes) (redactStmt no)
-                (While cond body) -> While cond $ redactStmt body
-                (BasicFor a b c body) -> BasicFor a b c $ redactStmt body
-                (EnhancedFor a b c d body) -> EnhancedFor a b c d $ redactStmt body
-                (ExpStmt exp) -> ExpStmt $ redactExp patch exp
-                (Switch cond blocks) -> Switch cond $ map redactSwitchBlock blocks
-                    where redactSwitchBlock (SwitchBlock label stmts) = SwitchBlock label $ map (redactBlockStmt patch) stmts
-                (Do stmt cond) -> Do (redactStmt stmt) cond
-                (Return (Just exp)) -> Return $ Just $ redactExp patch exp
-                (Try tryBlock catches maybeFinallyBlock) -> Try (redactBlock patch tryBlock) catches (redactBlock patch <$> maybeFinallyBlock)
-                (Labeled a stmt) -> Labeled a $ redactStmt stmt
-                _ -> block
+    (BlockStmt stmt) -> BlockStmt $ redactStmt stmt where
+        redactStmt block = case block of
+            (StmtBlock block) -> StmtBlock $ redactBlock patch block
+            (IfThen cond stmt) -> IfThen cond $ redactStmt stmt
+            (IfThenElse cond yes no) -> IfThenElse cond (redactStmt yes) (redactStmt no)
+            (While cond body) -> While cond $ redactStmt body
+            (BasicFor a b c body) -> BasicFor a b c $ redactStmt body
+            (EnhancedFor a b c d body) -> EnhancedFor a b c d $ redactStmt body
+            (ExpStmt exp) -> ExpStmt $ redactExp patch exp
+            (Switch cond blocks) -> Switch cond $ map redactSwitchBlock blocks
+                where redactSwitchBlock (SwitchBlock label stmts) = SwitchBlock label $ map (redactBlockStmt patch) stmts
+            (Do stmt cond) -> Do (redactStmt stmt) cond
+            (Return (Just exp)) -> Return $ Just $ redactExp patch exp
+            (Try tryBlock catches maybeFinallyBlock) -> Try (redactBlock patch tryBlock) catches (redactBlock patch <$> maybeFinallyBlock)
+            (Labeled a stmt) -> Labeled a $ redactStmt stmt
+            _ -> block
     (LocalVars modifiers varType varDecls) ->
-        LocalVars modifiers varType $ map redactVarDecl varDecls
-        where redactVarDecl decl@(VarDecl declId varInit) =
+        LocalVars modifiers varType $ map redactVarDecl varDecls where
+            redactVarDecl decl@(VarDecl declId varInit) =
                 case varInit of
                     Nothing -> decl
                     Just varInit -> VarDecl declId $ Just $ redactVarInit patch varInit
